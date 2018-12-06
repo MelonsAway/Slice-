12240e614b2e5c0118112bbb9e85166d06043dfa
Zane Waite <zmwaite182@stevenscollege.edu>
2018-12-06 15:44:55 -0500

    added passing test for addOrder function

--- main.test.js
+++ main.test.js
@@ -1,1 +1,15 @@
-const {} = require('./main');
+const {addOrder} = require('./main');
+
+describe('add order', () => {
+  test('an order should be added to any existing items', () => {
+    //there should be one order in a list of orders
+    let orders = [{number : 3, ingredient : 'pepperoni'}];
+    const order = {number : 5, ingredient : 'pepperoni'};
+    expect(orders.length).toBe(1);
+    //run function
+    addOrder(order, orders);
+    //there should be 2 orders in correct order
+    expect(orders.length).toBe(2);
+    expect(orders[1]).toBe(order);
+  });
+});
